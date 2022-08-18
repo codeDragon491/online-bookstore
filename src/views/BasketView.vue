3c108ac4445511f833b2e68d6d6f2c0b99c31f7f
codeDragon491 <trefasjulia@gmail.com>
2022-08-18 08:01:35 +0200

    Display data from item API

--- src/views/BasketView.vue
+++ src/views/BasketView.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="basket page">
+     <section data-cy="items" class="items">
+      <div data-cy="item" class="item-wrapper" v-for="item in items" :key="item.id">
+        <img :src="item.image_url" :alt="items.title" />
+        <div class="info-box">
+          <h2 class="title">{{ item.title}}</h2>
+          <p class="price">Price: <span>{{ item.price }} €</span></p>
+          <p class="quantity">Quantity: <span> {{ item.quantity }}</span></p>
+          <div class="button">
+            <label for="item-select">
+              <select v-model="item.quantity" name="item-select">
+                <option v-for="option in options" :key="option.value" :value="option.value">
+                  {{ option.text }}
+                </option>
+              </select>
+            </label>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section data-cy="error" class="error">
+      {{ error }}
+    </section>
   </div>
 </template>
 
@@ -15,6 +37,8 @@ export default defineComponent({
   components: {
   },
   setup() {
+    // only mock data for options
+    const options = ref([{ value: 1, text: 'Change quantity' }, { value: 1, text: 1 }, { value: 2, text: 2 }, { value: 3, text: 3 }])
     const {
       loading, items, itemsInBasket, error, getItem,
     } = useItems()
@@ -30,7 +54,7 @@ export default defineComponent({
     })
 
     return {
-      items, error,
+      items, error, options,
     }
   },
 });
