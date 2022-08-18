c6c60e314cf8cf251b6a6c9bee3f3fb27cddd488
codeDragon491 <trefasjulia@gmail.com>
2022-08-18 17:28:38 +0200

    Add firebase deployment settings

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBzfW5ztD3i8OjJUNvc6adJwXFdWhAYajQ",
+  authDomain: "online-bookstore-97d53.firebaseapp.com",
+  projectId: "online-bookstore-97d53",
+  storageBucket: "online-bookstore-97d53.appspot.com",
+  messagingSenderId: "595269466495",
+  appId: "1:595269466495:web:c493cd005a33dd9290424a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
